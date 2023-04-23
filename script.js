0e39648a747eda9b318396e51b9b6154b03e2575
polashbiswas <30291056+polashbiswas@users.noreply.github.com>
2023-04-23 15:47:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let student = {
+	name: "avi"
+}
+Object.prototype.getKeys = function () {
+  let arr = [];
+  for (let key in student) {
+    arr.push(key)
+  }
+  return arr;
+}
